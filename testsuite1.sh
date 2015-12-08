abaf1bf52def8426c2c8268d70d779ffc1ef64da
francisypl <francisypl@gmail.com>
2015-12-08 11:28:59 -0800

    testsuite1

---
+++ testsuite1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python search.py oil4.txt
+python search.py oil5.txt
+python search.py oil6.txt
+python search.py oil7.txt
+python search.py oil8.txt
+python search.py oil9.txt
+python search.py oil10.txt
+
+python search.py chemical.txt
+python search.py chemical2.txt
+python search.py chemical3.txt
+python search.py chemical4.txt
+python search.py chemical5.txt
+python search.py chemical6.txt
+python search.py chemical7.txt
+python search.py chemical8.txt
+python search.py chemical9.txt
+python search.py chemical10.txt
